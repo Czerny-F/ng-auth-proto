d31f9a42e5549e1c1caff92b36f1fb7ae7e94d79
Czerny-F <dummy00.cz@gmail.com>
2018-10-05 20:13:00 +0900

    auth.service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@angular/core';
 import { Router } from "@angular/router";
 import { NgRedux } from '@angular-redux/store';
 import { Observable, of } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 
 import { IAppState } from '../redux/store';
+import { AuthActions } from '../redux/actions';
 
 
 @Injectable({
@@ -11,23 +13,59 @@ import { IAppState } from '../redux/store';
 })
 export class AuthService {
   authenticated$;
-
   isAuthenticated = false;
+
+  loginUrl = '/login';
+  redirectUrl = '/profile';
 
   constructor(
     private ngRedux: NgRedux<IAppState>,
+    private actions: AuthActions,
     private router: Router
   ) {
     this.authenticated$ = ngRedux.select<boolean>('authenticated')
       .subscribe(authed => this.isAuthenticated = authed);
   }
 
-  check(): Observable<boolean> {
-    if (this.isAuthenticated) {
-      return of(true);
+  getPermission(path: string): void {
+    console.log(this.isAuthenticated);
+    if (this.isAuthenticated && path === this.loginUrl) {
+      this.router.navigate([this.redirectUrl]);
+    }
+    if (!this.isAuthenticated && path !== this.loginUrl) {
+      this.router.navigate([this.loginUrl]);
+    }
+  }
+
+  login(credentials: object): Observable<string> {
+    console.log(credentials);
+
+    if (this.authenticate(credentials)) {
+      console.log(this.ngRedux.dispatch(this.actions.login()));
+
+      return of('OK').pipe(
+        tap(_ => this.router.navigate([this.redirectUrl])),
+        catchError(this.handleError<string>('login', 'Failed'))
+      );
     }
 
-    this.router.navigate(['/login']);
-    return of(false);
+    return of('Login failed');
+  }
+
+  logout(): void {
+    this.ngRedux.dispatch(this.actions.logout());
+    this.router.navigate([this.loginUrl]);
+  }
+
+  private authenticate(credentials: object): boolean {
+    // anyway
+    return true;
+  }
+
+  private handleError<T>(operation: string, result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
   }
 }
